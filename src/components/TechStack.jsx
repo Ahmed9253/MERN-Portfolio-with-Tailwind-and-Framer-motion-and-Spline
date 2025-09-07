b1f2afa1594b0d18db1a68b0f7b879c308e20418
Muhammad Ahmed <ahmedjutt9253@gmail.com>
2025-09-07 15:54:15 +0500

    three sections done

--- src/components/TechStack.jsx
+++ src/components/TechStack.jsx
@@ -15,6 +15,10 @@ const TechIcons = [
     icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/cplusplus/cplusplus-original.svg",
   },
   {
+    name: "C",
+    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/c/c-original.svg",
+  },
+  {
     name: "HTML",
     icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg",
   },
